8ed0a0943490d5986b7027be3b5a42b1482287a6
AD-SHAZAD <167025579+AD-SHAZAD@users.noreply.github.com>
2024-05-29 18:03:30 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,9 @@
-var Score = 1000;
+var Score = 2000;
 let startTime;
 var __ended = false
 document.getElementById('h1').innerHTML = Score;
 function Button_Click(){
-    if (Score === 1000 && startTime === undefined) { startTime = Date.now(); }
+    if (Score === 2000 && startTime === undefined) { startTime = Date.now(); }
     if (Score === 1) { 
         document.getElementById('h1').style.fontSize = '30%';
         // Calculate and display the elapsed time
