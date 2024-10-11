fc2c208dcceec24aa94cdb57d0c6d3ae7a7a7357
AD-SHAZAD <167025579+AD-SHAZAD@users.noreply.github.com>
2024-10-11 22:40:14 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,9 @@
-var Score = 2000;
+var Score = 1500;
 let startTime;
 var __ended = false
 document.getElementById('h1').innerHTML = Score;
 function Button_Click(){
-    if (Score === 2000 && startTime === undefined) { startTime = Date.now(); }
+    if (Score === 1500 && startTime === undefined) { startTime = Date.now(); }
     if (Score === 1) { 
         document.getElementById('h1').style.fontSize = '30%';
         // Calculate and display the elapsed time
@@ -11,7 +11,7 @@ function Button_Click(){
         const elapsedTime = (endTime - startTime) / 1000; // convert milliseconds to seconds
 
         console.log(`Time taken: ${elapsedTime} seconds`);
-        document.getElementById('h1').innerHTML = `<p> This is how to keep an idiot busy for <span> ${ Math.floor( elapsedTime ) }s </span> </p>`;
+        document.getElementById('h1').innerHTML = `<p> Wasted <span> ${ Math.floor( elapsedTime ) }s </span> succesfully </p>`;
         __ended = true
         // Reset startTime for future use if needed
         startTime = undefined;
